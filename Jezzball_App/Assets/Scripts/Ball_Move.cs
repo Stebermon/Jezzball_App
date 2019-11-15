8c562e35d5a3b2fd3417191d5daeb40dcee2e15d
steven <snesmith@asu.edu>
2019-11-14 20:59:33 -0700

    Updated to add background cell grid spawner and ball spawner

---
+++ Jezzball_App/Assets/Scripts/Ball_Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Move : MonoBehaviour
+{
+    private Rigidbody2D balls_rb;
+    public GameObject gameBalls;
+    public int numBalls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera camera = Camera.main;
+        for (int i = 0; i < numBalls; i++)
+        {
+            Instantiate(gameBalls, camera.ViewportToWorldPoint(new Vector3(Random.Range(0, 1), Random.Range(0, 1), 9)), Quaternion.identity);
+        }
+        
+        GameObject[] balls = GameObject.FindGameObjectsWithTag("Balls");
+
+        for (int i = 0; i < balls.Length; i++)
+        {
+            balls_rb = balls[i].GetComponent<Rigidbody2D>();
+        
+            // Move ball up 10 units per second
+            balls_rb.velocity = new Vector2(Random.Range(-10, 10), Random.Range(-10,10));
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Jezzball_App/Assets/Scripts/GridManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int colLen, rowLen;
+    public float xSpace, ySpace;
+    public GameObject blocks;
+    public float xStart, yStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(blocks.GetComponent<SpriteRenderer>().bounds.size);
+        for (int i = 0; i < colLen * rowLen; i++)
+        {
+            Instantiate(blocks, new Vector3(xStart + (xSpace * (i % colLen)), yStart + (-ySpace * (i / colLen))), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
