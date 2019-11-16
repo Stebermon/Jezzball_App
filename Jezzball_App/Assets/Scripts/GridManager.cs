23c39990baab0a2c610e8cbc1e996bcc5ebacca7
jtborn <jt_born@hotmail.com>
2019-11-16 10:20:33 -0700

    Fixed cube spawner script

--- Jezzball_App/Assets/Scripts/GridManager.cs
+++ Jezzball_App/Assets/Scripts/GridManager.cs
@@ -4,27 +4,30 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    public int colLen, rowLen;
+    public int colNum, rowNum;
     public GameObject blocks;
     private float xStart, yStart;
     private float xSpace, ySpace;
     // Start is called before the first frame update
     void Start()
     {
-        xSpace = 1.5f;
-        ySpace = 1.4f;
+        xSpace = 1f;
+        ySpace = 1f;
         var cam = Camera.main;
         var topLeft = (Vector2)cam.ScreenToWorldPoint(new Vector3(0, cam.pixelHeight, cam.nearClipPlane));
+        var topRight = (Vector2)cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight, cam.nearClipPlane));
+
         // Add half the size of the block to topLeft coordinate to position the sprite perfectly at the edge
         xStart = topLeft.x + xSpace / 2;
         yStart = topLeft.y + ySpace / 2;
-        //transform.localScale
 
-        Debug.Log(xStart);
+
+        Debug.Log(xSpace);
         Debug.Log(blocks.GetComponent<SpriteRenderer>().bounds.size);
-        for (int i = 0; i < colLen * rowLen; i++)
+        for (int i = 0; i < colNum * rowNum; i++)
         {
-            Instantiate(blocks, new Vector3(xStart + (xSpace * (i % colLen)), yStart + (-ySpace * (i / colLen))), Quaternion.identity);
+            Instantiate(blocks, new Vector3(xStart + (xSpace * (i % colNum)), yStart + (-ySpace * (i / colNum))), Quaternion.identity);
+            //block.transform.localScale = new Vector2(1, 1);
         }
     }
 
