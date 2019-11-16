6059ecb870d001291844de39e1651f9ef559a4b6
steven <snesmith@asu.edu>
2019-11-16 11:20:35 -0700

    Updated to stop balls from getting stuck in corners and not slow down when hitting eachother

---
+++ Jezzball_App/Assets/Scripts/Collision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private float speed;
+    private Vector2 unit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = gameObject.GetComponent<Rigidbody2D>().velocity.magnitude;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        unit = GetComponent<Rigidbody2D>().velocity.normalized;
+    }
+
+    // Collision
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Balls")
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = -unit * speed;
+        }
+    }
+}
