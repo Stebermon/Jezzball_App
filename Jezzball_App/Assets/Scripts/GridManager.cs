702af4e79d6421db046903fb48f0dba6a5de2003
jtborn <jt_born@hotmail.com>
2019-11-15 17:25:14 -0700

    Updated Game Manager script
    Gets screen bounds and spawns blocks starting at exact screen dimensions. Still need to properly resize sprite to fit

--- Jezzball_App/Assets/Scripts/GridManager.cs
+++ Jezzball_App/Assets/Scripts/GridManager.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     public int colLen, rowLen;
-    public float xSpace, ySpace;
     public GameObject blocks;
-    public float xStart, yStart;
+    private float xStart, yStart;
+    private float xSpace, ySpace;
     // Start is called before the first frame update
     void Start()
     {
+        xSpace = 1.5f;
+        ySpace = 1.4f;
+        var cam = Camera.main;
+        var topLeft = (Vector2)cam.ScreenToWorldPoint(new Vector3(0, cam.pixelHeight, cam.nearClipPlane));
+        // Add half the size of the block to topLeft coordinate to position the sprite perfectly at the edge
+        xStart = topLeft.x + xSpace / 2;
+        yStart = topLeft.y + ySpace / 2;
+        //transform.localScale
+
+        Debug.Log(xStart);
         Debug.Log(blocks.GetComponent<SpriteRenderer>().bounds.size);
         for (int i = 0; i < colLen * rowLen; i++)
         {
